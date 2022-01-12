0f2cfd234accdb894c0f244eca4e3bed5480a65f
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2022-01-12 20:38:37 +0000

    i figured out life in this code

---
+++ LifeSummarized.java
@@ -0,0 +1,15 @@
+class Life {
+	public static void main(String args[])
+	{
+		boolean thelife = true;
+		boolean failure = false;
+
+		while (thelife == true) {
+			System.out.println("Decide what's best to be done");
+			System.out.println("Do what is decided");
+			if (failure == true) {
+				break
+			}
+		}
+	}
+}
